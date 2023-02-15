615bde1b7565dc8a32099f2773b666151ad86a1f
keremcengiz0 <keremcengiz0@hotmail.com>
2023-02-15 23:58:41 +0300

    added dialog(pop-up) for delete customer.

--- src/components/Customer/GetCustomer.js
+++ src/components/Customer/GetCustomer.js
@@ -12,6 +12,13 @@ import Container from "@mui/material/Container";
 import DeleteIcon from "@mui/icons-material/Delete";
 import IconButton from "@mui/material/IconButton";
 import CreateIcon from "@mui/icons-material/Create";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
+import Button from "@mui/material/Button";
 
 const useStyles = makeStyles({
   table: {
@@ -26,9 +33,23 @@ function GetCustomer() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [error, setError] = useState(null);
   const [customers, setCustomers] = useState([]);
+  const [customerId, setCustomerId] = useState(null);
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
 
-  const deleteCustomer = (customerId) => {
+  const handleClickOpen = (id) => {
+    setCustomerId(id);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setCustomerId(null);
+  };
+
+  const deleteCustomer = () => {
     axios
       .delete(`/api/v1/customers/${customerId}`)
       .then((response) => {
@@ -104,13 +125,37 @@ function GetCustomer() {
                   </TableCell>
                   <TableCell align="center">{customer.birthDate}</TableCell>
                   <TableCell align="center">
-                    <IconButton
-                      onClick={() => {
-                        deleteCustomer(customer.id);
-                      }}
-                    >
+                    <IconButton onClick={() => handleClickOpen(customer.id)}>
                       <DeleteIcon />
                     </IconButton>
+                    <Dialog
+                      fullScreen={fullScreen}
+                      open={open}
+                      onClose={handleClose}
+                      aria-labelledby="responsive-dialog-title"
+                    >
+                      <DialogContent>
+                        <DialogContentText>
+                          <b style={{ fontSize: 24 }}>
+                            Are you sure you want to delete the customer?
+                          </b>
+                        </DialogContentText>
+                      </DialogContent>
+                      <DialogActions>
+                        <Button autoFocus onClick={handleClose}>
+                          <b>Cancel</b>
+                        </Button>
+                        <Button
+                          onClick={() => {
+                            handleClose();
+                            deleteCustomer();
+                          }}
+                          autoFocus
+                        >
+                          <b style={{ color: "red" }}>Delete</b>
+                        </Button>
+                      </DialogActions>
+                    </Dialog>
                     <IconButton
                       onClick={() => {
                         window.location = `/api/v1/customers/update/${customer.id}`;
